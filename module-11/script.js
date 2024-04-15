620d4dfd25b284a64b7ff5b71139dc8fff73ce46
Irfan Shaik <161253393+irfan3110@users.noreply.github.com>
2024-04-14 22:10:43 -0700

    Add files via upload

---
+++ module-11/script.js
@@ -0,0 +1,25 @@
+document
+  .getElementById("newCatButton")
+  .addEventListener("click", loadRandomCat);
+
+function loadRandomCat() {
+  fetch("https://api.thecatapi.com/v1/images/search")
+    .then((response) => response.json())
+    .then((data) => {
+      const catImageContainer = document.getElementById("catImageContainer");
+      if (data.length > 0 && data[0].url) {
+        catImageContainer.innerHTML = `<img src="${data[0].url}" alt="A random cat">`;
+      } else {
+        catImageContainer.innerHTML = "<p>No cats found :(</p>";
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+      const catImageContainer = document.getElementById("catImageContainer");
+      catImageContainer.innerHTML =
+        "<p>Error loading cat picture. Try again later.</p>";
+    });
+}
+
+// Initially load a random cat image
+loadRandomCat();
